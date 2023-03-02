2fa63e9030f03c7e9985aea7c679f5090bfff5a7
SwimStar27 <94149915+SwimStar27@users.noreply.github.com>
2023-03-01 19:42:25 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+x = 0;
+y = 0;
+rectangle = "";
+dcircle = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start(){
+    document.getElementById("status").innerHTML="Say Circle Or Rectangle !!    ";
+    recognition.start();
+}
+
+recognition.onresult = function(event){
+    console.log(event);
+    cookie = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML="Did You Say:    "+cookie;
+    if(cookie == "circle"|| cookie=="Circle"){
+        x=Math.floor(Math.random()*350);
+        y=Math.floor(Math.random()*350);
+        document.getElementsById("status").innerHTML="Drawing A Circle Now !!    ";
+        dcircle="dessert";
+    }
+    if(cookie == "rectangle"|| cookie=="Rectangle"){
+        x=Math.floor(Math.random()*350);
+        y=Math.floor(Math.random()*350);
+        document.getElementsById("status").innerHTML="Drawing A Rectangle Now !!    ";
+        rectangle="dessert";
+    }
+}
+
+
+function setup(){
+    canvas = createCanvas(400,400)
+}
+
+function draw(){
+     if(dcircle == "dessert"){
+        radius=Math.floor(Math.random()*120);
+        circle(x,y,radius);
+        document.getElementById("status").innerHTML="Masterpiece !!   ";
+     }
+     if(rectangle == "dessert"){
+        rect(x,y,80,60);
+        document.getElementById("status").innerHTML="Masterpiece !!   ";
+     }
+}
