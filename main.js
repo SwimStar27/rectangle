f6ff92a0a5be13addff0aea05ce50fdc9e201771
SwimStar27 <94149915+SwimStar27@users.noreply.github.com>
2023-03-07 20:45:31 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,39 @@
-x = 0;
-y = 0;
-rectangle = "";
-dcircle = "";
-
-var SpeechRecognition = window.webkitSpeechRecognition;
-
-var recognition = new SpeechRecognition();
-
-function start(){
-    document.getElementById("status").innerHTML="Say Circle Or Rectangle !!    ";
-    recognition.start();
-}
-
-recognition.onresult = function(event){
-    console.log(event);
-    cookie = event.results[0][0].transcript;
-
-    document.getElementById("status").innerHTML="Did You Say:    "+cookie;
-    if(cookie == "circle"|| cookie=="Circle"){
-        x=Math.floor(Math.random()*350);
-        y=Math.floor(Math.random()*350);
-        document.getElementById("status").innerHTML="Drawing A Circle Now !!    ";
-        dcircle="dessert";
-    }
-    if(cookie == "rectangle"|| cookie=="Rectangle"){
-        x=Math.floor(Math.random()*350);
-        y=Math.floor(Math.random()*350);
-        document.getElementById("status").innerHTML="Drawing A Rectangle Now !!    ";
-        rectangle="dessert";
-    }
-}
-
+nosex=0;
+nosey=0;
+left=0;
+right=0;
+difference=0;
 
 function setup(){
-    canvas = createCanvas(400,400)
+    video=createCapture(VIDEO);
+    video.size(500,500);
+
+    canvas=createCanvas(500,500);
+    canvas.position(670,280);
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Model Has Loaded !!");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+console.log(results);
+nosex=results[0].pose.nose.x;
+nosey=results[0].pose.nose.y;
+console.log("Nose X =  " + nosex +"Nose Y =  " + nosey);
+left=results[0].pose.leftWrist.x;
+right=results[0].pose.rightWrist.x;
+difference=floor(left-right);
+console.log("Left Wrist = "+left +"Right Wrist = "+right+"Difference = "+difference);
+    }
 }
 
 function draw(){
-     if(dcircle == "dessert"){
-        radius=Math.floor(Math.random()*120);
-        circle(x,y,radius);
-        document.getElementById("status").innerHTML="Masterpiece !!   ";
-     }
-     if(rectangle == "dessert"){
-        rect(x,y,80,60);
-        document.getElementById("status").innerHTML="Masterpiece !!   ";
-     }
+background("#90d6d3");
+fill("#d7b0ff");
+stroke("#ffffed");
+square(nosex,nosey,difference);
 }
